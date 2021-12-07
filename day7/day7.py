a7cbf85ba62e5991841f7904c95843bbea531e2a
Hugo DETANG <hugo.detang@bedrockstreaming.com>
2021-12-07 08:52:59 +0100

    Day 7 Challenge

---
+++ day7/day7.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+def getCost(positions, expensive = False):
+    cheapestCost = None
+    
+    for index in range(max(positions) + 1):
+        cost = 0
+        for position in positions:
+            steps = abs(position - index)
+            cost += steps if expensive == False else int(steps * (steps + 1) / 2)
+
+        if cheapestCost == None or cost < cheapestCost:
+            cheapestCost = cost
+        
+    return cheapestCost
+
+if __name__ == '__main__':
+    with open('./input.txt', 'r') as file:
+        input = file.readlines()
+
+    positions = np.array(input[0].split(','))
+    positions = positions.astype(int)
+
+    print(getCost(positions))
+    print(getCost(positions, True))
