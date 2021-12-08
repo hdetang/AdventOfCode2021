cd621ce43f83d687f55c496c9de84cbb25ff975e
unknown <hdetang@hotmail.fr>
2021-12-09 00:55:25 +0100

    Day 8 Challenge Step 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,13 +1,88 @@
-import numpy as np
+def getMapping(patterns):
+    digits = {}
+
+    fiveDigitPatterns = []
+    sixDigitPatterns = []
+
+    for pattern in patterns:
+        sortedPattern = ''.join(sorted(pattern))
+
+        match len(sortedPattern):
+            case 2:
+                digits['1'] = sortedPattern
+            case 3:
+                digits['7'] = sortedPattern
+            case 4: 
+                digits['4'] = sortedPattern
+            case 5:
+                fiveDigitPatterns.append(sortedPattern)
+            case 6:
+                sixDigitPatterns.append(sortedPattern)
+            case 7: 
+                digits['8'] = sortedPattern
+    
+    almostNine = list(set(digits['7']) - set(digits['1']))[0] + digits['4']
+    letterG = None
+    for pattern in sixDigitPatterns:
+        difference = list(set(pattern) - set(almostNine))
+        if len(difference) == 1:
+            digits['9'] = pattern
+            letterG = difference[0]
+            sixDigitPatterns.remove(digits['9'])
+            break
+
+    for pattern in sixDigitPatterns:
+        difference = list(set(digits['9']) - set(pattern))
+        if len(difference) == 0: continue
+        if len(difference) == 1 and difference[0] in digits['1']:
+            digits['6'] = pattern
+            sixDigitPatterns.remove(digits['6'])
+            break
+
+    digits['0'] = sixDigitPatterns[0]
+
+    almostThree = ''.join(sorted(digits['7'] + letterG))    
+    for pattern in fiveDigitPatterns:
+        if len(set(pattern) - set(almostThree)) == 1:
+            digits['3'] = pattern
+            fiveDigitPatterns.remove(digits['3'])
+            break
+
+    for pattern in fiveDigitPatterns:
+        difference = list(set(digits['9']) - set(pattern))
+        if len(difference) == 0: continue
+        if len(difference) == 1 and difference[0] in digits['1']:
+            digits['5'] = pattern
+            fiveDigitPatterns.remove(digits['5'])
+            break
+    
+    digits['2'] = fiveDigitPatterns[0]
+
+    return {value: key for key, value in digits.items()}
 
 def step1(entries):
     count = 0
 
     for entry in entries:
-        patterns, outputs = entry
+        _, outputs = entry
         for output in outputs:
             if len(output) in [2, 4, 3, 7]:
                 count += 1
+    return count
+
+def step2(entries):
+    count = 0
+
+    for entry in entries:
+        patterns, outputs = entry
+        mapping = getMapping(patterns)
+        decoded = ''
+    
+        for output in outputs:
+            decoded += mapping[''.join(sorted(output))]
+        
+        count += int(decoded)
+
     return count
 
 if __name__ == '__main__':
@@ -18,7 +93,8 @@ if __name__ == '__main__':
     raw = [item.replace('\n', '') for item in input]
 
     for entry in raw:
-        patterns, ouput = entry.split('|')
-        entries.append([patterns.strip().split(' '), ouput.strip().split(' ')])
-    
+        patterns, ouputs = entry.split('|')
+        entries.append([patterns.strip().split(' '), ouputs.strip().split(' ')])
+
     print(step1(entries))
+    print(step2(entries))
